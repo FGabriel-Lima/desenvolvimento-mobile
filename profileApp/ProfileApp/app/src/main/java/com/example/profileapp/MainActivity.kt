dfcdb8d99dd810ee39e1f942a3206bcac7bf8a35
Gabriel Lima <gabrelgabrel621@gmail.com>
2024-11-16 14:08:23 -0300

    Adicionando profileApp

---
+++ profileApp/ProfileApp/app/src/main/java/com/example/profileapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.profileapp
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val profileImage = findViewById<ImageView>(R.id.profileImage)
+        val nameText = findViewById<TextView>(R.id.nameText)
+        val descriptionText = findViewById<TextView>(R.id.descriptionText)
+        val titlesWinner = findViewById<TextView>(R.id.titlesWinner)
+
+        profileImage.setOnClickListener{
+            Toast.makeText(this, "Logo do São Paulo Futebol Clube",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+}
