106dc6281f2956a3cc8ca7ba25f6f8f1ed7b63c2
FGabriel-Lima <fgabriellimace@gmail.com>
2025-01-23 17:06:45 -0300

    Adicionando MsgApp

---
+++ MsgApp2/app/src/main/java/com/example/msgapp2/ui/view/MessageApp.kt
@@ -0,0 +1,48 @@
+package com.example.msgapp2.ui.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MessageApp() {
+}
+
+@Composable
+fun MessageBubble(content: String, isUserMessage: Boolean) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = if (isUserMessage) Arrangement.End else Arrangement.Start
+    ) {
+        Box(
+            modifier = Modifier
+                .background(
+                    color = if (isUserMessage) MaterialTheme.colorScheme.primary else {
+                        MaterialTheme.colorScheme.surface
+                    },
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .padding(12.dp)
+                .widthIn(max = 250.dp)
+        ) {
+            Text(
+                text = content,
+                style = MaterialTheme.typography.bodyMedium.copy(
+                    color = if (isUserMessage) MaterialTheme.colorScheme.onPrimary else {
+                        MaterialTheme.colorScheme.onSurface
+                    }
+                )
+            )
+        }
+    }
+}
